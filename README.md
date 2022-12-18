9d9cce088295dfb34d9b89660ef8a324397d2ec6
nicolasbuchwalder <okkin9991@gmail.com>
2022-12-18 02:12:51 -0500

    add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# MTH9815 Final 
+### @ Nicolas Buchwalder
+
+## Setup and Installation
+The entire project can be compiled and run with command:
+```
+    ./setup.sh
+```
+You can add arguments
+    -r,--recompile    delete executables (if they exist) and recompile before launching
+    -d,--deleteafter  delete the executables after running
+    -b,--both         do both
+    -h,--help         gives help
+
+**WARNING:** this has only be tested on macos, if the setup does not work, please do the following commands:
+
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# list of files to run in correct order
 files=(\
     "./dataproviders/bondinquiryprovider/bondinquiryprovider" \
     #"./dataproviders/bondmarketdataprovider/bondmarketdataprovider" \
@@ -10,12 +11,13 @@ files=(\
     "./tradinglogs/bondstreaminglog/bondstreaminglog" \
     )
 
+# runs make to compile if necessary
 function makefiles() {
     echo "running make files to compile project if necessary"
     make
 }
 
-# macos new terminal and run
+# macos new terminal and run script in 1st argument
 function macosrunfile() {
 
     osascript \
@@ -27,64 +29,90 @@ function macosrunfile() {
     -- "$1"
 }
 
-
-# linux new terminal and run
+# linux new terminal and run script in 1st argument
 function linuxrunfile() {
     x-terminal-emulator -e "$1"
 }
 
+# function to run in mac
 function macoslaunch() {
     for ((i = 0; i < ${#files[@]}; i++)); do
-	echo "${file}"
         macosrunfile "${files[$i]}"
         sleep 0.1
     done
 }
 
+# function to run in linux
 function linuxlaunch() {
-    for exec in ${files[@]}; do
-        linuxrunfile ${exec}
+    for ((i = 0; i < ${#files[@]}; i++)); do
+        linuxrunfile "${files[$i]}"
+        sleep 0.1
     done
 }
 
+# main function
+function launch() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "detected macos"
+        
+        makefiles
+        if [ $? -eq 0 ]; then
+            echo "compile successful"
+            echo "launching all processes"
+            macoslaunch
+        else
+            echo "compile unsuccessful, please check errors"
+        fi
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "detected macos"
-    
-    makefiles
-    if [ $? -eq 0 ]; then
-        echo "compile successful"
-        echo "launching all processes"
-        macoslaunch
     else
-        echo "compile unsuccessful, please check errors"
+        echo "detected linux"
+        makefiles
+        if [ $? -eq 0 ]; then
+            echo "compile successful"
+            echo "launching all processes"
+            macoslaunch
+        else
+            echo "compile unsuccessful, please check errors"
+        fi
     fi
+}
 
-else
-    echo "detected linux"
-    makefiles
-    if [ $? -eq 0 ]; then
-        echo "compile successful"
-        echo "launching all processes"
-        macoslaunch
-    else
-        echo "compile unsuccessful, please check errors"
+function delete_execs(){
+    if [ -f "$file" ] ; then
+        rm "$file"
     fi
-fi
+}
+function help() {
+    echo "commands:"
+    echo "    -r,--recompile    delete executables (if they exist) and recompile before launching"
+    echo "    -d,--deleteafter  delete the executables after running"
+    echo "    -b,--both         do both"
+    echo "    -h,--help         gives help"
+}
 
  
-
-
-
-#$macosrun ./dataproviders/bondinquiryprovider/bondinquiryprovider
-#${runscript} #./tradingsystem/tradingsystem
-#(cd dataproviders/bondinquiryprovider; ./bondinquiryprovider) &
-#(cd dataproviders/bondmarketdataprovider; ./bondmarketdataprovider) &
-#(cd dataproviders/bondtradebookingprovider; ./bondtradebookingprovider) &
-#(cd dataproviders/bondpricingprovider; ./bondpricingprovider) &
-#(cd tradinglogs/bondinquiryprovider; ./bondinquiryprovider) &
-#(cd tradingsystem; ./tradingsystem) &
-#fg
+case $1 in
+--recompile|-r)
+delete_execs
+launch
+;;
+--deleteafter|-d)
+launch
+delete_execs
+;;
+--both|-b
+delete_execs
+launch
+delete_execs
+;;
+--help|-h)
+help
+;;
+*)
+launch
+;;
+esac
+exit 0
 
 
 
